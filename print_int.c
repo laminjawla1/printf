89f67560b61e9724a4487113ba979f18fa499f04
Lamin Jawla <laminjawla1001@gmail.com>
2023-04-23 04:23:54 +0000

    print int

--- print_int.c
+++ print_int.c
@@ -26,5 +26,34 @@ int helper_print_int(int n, int i)
 */
 int print_int(va_list args)
 {
-	return (helper_print_int(va_arg(args, int), 0));
+	int signed_number, highest_power, count = 0, check;
+	unsigned int unsigned_number;
+
+	signed_number = va_arg(args, int);
+
+	if (signed_number < 0)
+	{
+		check = _putchar('-');
+		unsigned_number = -signed_number;
+		if (check > 0)
+			count += check;
+		else
+			return (count);
+	}
+	else
+		unsigned_number = signed_number;
+
+	highest_power = 1;
+	for (; unsigned_number / highest_power > 9; highest_power *= 10)
+		;
+
+	while (highest_power)
+	{
+		check = _putchar(unsigned_number / highest_power + '0');
+		if (check > 0)
+			count += check;
+		unsigned_number %= highest_power;
+		highest_power /= 10;
+	}
+	return (count);
 }
